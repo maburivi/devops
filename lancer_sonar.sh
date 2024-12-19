911c8f4884b4d8f2d07dda2b898aef74639635e7
maburivi <mathieubureau17@gmail.com>
2024-12-19 16:21:04 +0100

    Update lancer_sonar.sh

--- lancer_sonar.sh
+++ lancer_sonar.sh
@@ -1,5 +1,9 @@
-/home/kali/Desktop/sonar/sonar-scanner-6.2.1.4610-linux-x64/bin/sonar-scanner \  
-  -Dsonar.projectKey=testlocal \
+#!bin/bash
+
+/home/kali/Desktop/sonar_jenkins/sonar-scanner-6.2.1.4610-linux-x64/bin/sonar-scanner \
+  -Dsonar.projectKey=test_jenkins \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://172.17.0.1:9000 \
-  -Dsonar.token=sqp_c3ade41d8df8f98ec4d378552724ba702b85526e -Dsonar.exclusions=**/*.java
+  -Dsonar.token=sqp_e988127e1687611fac46066b9c0f01864789656f -Dsonar.exclusions=**/*.java
+
+echo "Scan sonarqube effectue"
