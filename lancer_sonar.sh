0e90471eea827a779098e10b24938de668d0c5c9
maburivi <mathieubureau17@gmail.com>
2024-12-20 11:24:31 +0100

    Update lancer_sonar.sh

--- lancer_sonar.sh
+++ lancer_sonar.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-/home/kali/Desktop/sonar_jenkins/sonar-scanner-6.2.1.4610-linux-x64/bin/sonar-scanner \
+/tmp/sonar/sonar_jenkins/sonar-scanner-6.2.1.4610-linux-x64/bin/sonar-scanner \
   -Dsonar.projectKey=test_jenkins \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://172.17.0.1:9000 \
